33dfbce882a927873836c5f3301cad9d648f27a0
virajith21 <virajithbhat21@gmail.com>
2026-08-18 18:36:24 +0530

    Component no. 3

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,9 +1,15 @@
 
+import { Welcome } from "./components/P1.jsx";
+import Greet from "./components/P2.jsx";
+import { MyMovies } from "./components/P3";
 function App() {
-  
   return (
-   <h1>Hello my first react app</h1>
-  )
+    <div>
+        <Welcome />
+        <Greet />
+        <MyMovies />
+    </div>
+  );
 }
 
-export default App
+export default App;
