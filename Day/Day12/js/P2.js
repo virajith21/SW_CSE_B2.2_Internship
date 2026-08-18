b6f373e6201631cf35cd83c9d28a6b658e910edf
virajith21 <virajithbhat21@gmail.com>
2026-08-18 18:19:47 +0530

    named export & default export

---
+++ Day/Day12/js/P2.js
@@ -0,0 +1,7 @@
+import { add,subtract } from "./P1.js"; //Named export
+import multiply from "./P3.js"; //default export
+
+console.log(add(40,60));
+console.log(subtract(90,60));
+
+console.log(multiply(60,60));
