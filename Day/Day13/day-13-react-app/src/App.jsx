d0b66b9b86e2dce6c9fabd94a2768aef47ee9e8a
virajith21 <virajithbhat21@gmail.com>
2026-08-19 16:50:29 +0530

    Parent component & child component in the same file

--- Day/Day13/day-13-react-app/src/App.jsx
+++ Day/Day13/day-13-react-app/src/App.jsx
@@ -1,15 +1,35 @@
 
 
-function App() {
- 
+import { Welcome } from "./components/P1";
+import { ProductCard } from "./components/P2";
+import headphones from "./assets/sony.jpg";
+import ButtonDemo from "./components/P3";
+import Counter from "./components/StateExampleOne";
+import ToggleText from "./components/StateExampleTwo";
+import NameInput from "./components/StateExampleThree";
+import "./App.css";
 
+function App() {
   return (
     <>
-      
+      <NameInput/>
 
-      
+      <ToggleText />
+
+      <Counter />
+
+      <Welcome name="virajith" />
+
+      <ButtonDemo />
+
+      <ProductCard
+        //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+        price={15999}
+        //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg"
+        image={headphones}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
