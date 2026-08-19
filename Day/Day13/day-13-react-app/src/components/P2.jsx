0f4217147f6a45e4da3138131b6d704568b1fdae
virajith21 <virajithbhat21@gmail.com>
2026-08-19 16:30:05 +0530

    default value for prop

---
+++ Day/Day13/day-13-react-app/src/components/P2.jsx
@@ -0,0 +1,9 @@
+export function ProductCard({name="New Product",price,image}) {
+    return(
+        <div className="card">
+            <img src={image} alt={name} />
+            <h4>{name}</h4>
+            <h6>Rs. {price}</h6>
+        </div>
+    )
+}
