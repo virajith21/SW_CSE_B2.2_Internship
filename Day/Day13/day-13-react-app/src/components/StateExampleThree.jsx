3cd44a14a85145e85e9accca8803e664dfab598f
virajith21 <virajithbhat21@gmail.com>
2026-08-19 16:53:13 +0530

    State eg 3

---
+++ Day/Day13/day-13-react-app/src/components/StateExampleThree.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function NameInput() {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <h5>Enter your name:</h5>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <p>Hello, {name || "Guest"}</p>
+    </div>
+  );
+}
+export default NameInput;
